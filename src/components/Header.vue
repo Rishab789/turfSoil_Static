9b3499b62850df0c747330a740fc597626e33839
Rishab789 <beraprakash456@gmail.com>
2025-01-15 20:17:18 +0530

    My first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+  
+    <header class="flex items-center justify-between container">
+       
+        <div>
+            <img src="./../assets/finalfo.png" width="100"/>
+            <p>Turf and Soil Diagnostics</p>
+
+        </div>
+        <nav class="">
+            <ul class="flex gap-5">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Contacts</a></li>
+                <li><a href="#">Sample Submittal</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Articles</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Lab Accreditation</a></li>
+            </ul>
+        </nav>
+    </header>
+
+
+</template>
+
